3188f1d74828250d5b8a77a3e1ac8bee728f5a68
KurtHsu <myohmy10420@gmail.com>
2019-11-09 17:04:29 +0800

    add api/v1/products_controller#create

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,21 @@
 class Api::V1::ProductsController < Api::V1::BaseController
   def index
     products = Product.all
-    render status: 200, json: { products: products }
+    render json: { products: products }, status: 200
+  end
+
+  def create
+    product = Product.new(product_params)
+    if product.save
+      render json: { stasut: 'ok' }, status: 200
+    else
+      render json: { errors: product.errors.full_messages }, status: 400
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :content, :price, :inventory)
   end
 end
--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -3,4 +3,42 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
     before { get :index }
     it { expect(response).to have_http_status(:success) }
   end
+
+  describe 'POST #create' do
+    it 'post validated params' do
+      before_count = Product.count
+
+      params = {
+        product: {
+          name: '商品',
+          content: '商品內文',
+          inventory: 1,
+          price: 100
+        }
+      }
+      post :create, params: params
+
+      expect(Product.count).to eq(before_count + 1)
+      expect(response).to have_http_status(:success)
+      expect(response.content_type).to eq "application/json"
+    end
+
+    it 'post invalidated params' do
+      before_count = Product.count
+
+      params = {
+        product: {
+          name: nil,
+          content: nil,
+          inventory: 'not integer',
+          price: 'not integer'
+        }
+      }
+      post :create, params: params
+
+      expect(Product.count).to eq(before_count)
+      expect(response).to have_http_status(400)
+      expect(response.content_type).to eq "application/json"
+    end
+  end
 end
